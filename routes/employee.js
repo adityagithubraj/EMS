b83b48e2408e8358c2393f59cff9f869e12b9a06
adityagithubraj <adityaraj9843@gmail.com>
2023-07-14 19:30:09 +0530

    add paginatn

--- routes/employee.js
+++ routes/employee.js
@@ -18,38 +18,54 @@ router.post('/', async (req, res) => {
   }
 });
 
-// Get all employees
-// router.get('/', async (req, res) => {
-//   try {
-//     const employees = await Employee.find();
-//     res.status(200).json(employees);
-//   } catch (error) {
-//     res.status(500).json({ message: 'Failed to fetch employees' });
-//   }
-// });
-///2nd get
+Get all employees
 router.get('/', async (req, res) => {
   const page = parseInt(req.query.page) || 1;
   const limit = 5;
   const skip = (page - 1) * limit;
 
   try {
-      const totalEmployees = await Employee.countDocuments();
-      const totalPages = Math.ceil(totalEmployees / limit);
+    const totalEmployees = await Employee.countDocuments();
+    const totalPages = Math.ceil(totalEmployees / limit);
 
-      const employees = await Employee.find()
-          .skip(skip)
+    const employees = await Employee.find()
+
+    .skip(skip)
           .limit(limit);
 
-      res.status(200).send({
-          page,
+    //res.status(200).json(employees);
+    res.status(200).send({
+      page,
           totalPages,
           employees,
-      });
-  } catch (err) {
-      res.status(500).send({ msg: err.message });
+    })
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to fetch employees' });
   }
 });
+///2nd get
+// router.get('/', async (req, res) => {
+//   const page = parseInt(req.query.page) || 1;
+//   const limit = 5;
+//   const skip = (page - 1) * limit;
+
+//   try {
+//       const totalEmployees = await Employee.countDocuments();
+//       const totalPages = Math.ceil(totalEmployees / limit);
+
+//       const employees = await Employee.find()
+//           .skip(skip)
+//           .limit(limit);
+
+//       res.status(200).send({
+//           page,
+//           totalPages,
+//           employees,
+//       });
+//   } catch (err) {
+//       res.status(500).send({ msg: err.message });
+//   }
+// });
 
 
 
